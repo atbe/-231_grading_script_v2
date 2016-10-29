145c969385b4c82d11a36801a1ea1bc51fa785a2
@be <abeahmed2@gmail.com>
2016-10-29 11:53:29 -0400

    Moved code to src folder, adapted shell script to build from there. Removed executable permission on Grader.py as it is no longer needed.

--- make_231Grader.sh
+++ make_231Grader.sh
@@ -1,2 +1,5 @@
 echo -e "Making new 231Grader.py file..."
-zip 231Grader.py Grader.py __main__.py Project.py Students.py Student.py Tools.py
+cd src
+zip ./231Grader.py Grader.py __main__.py Project.py Students.py Student.py Tools.py
+mv 231Grader.py ..
+
